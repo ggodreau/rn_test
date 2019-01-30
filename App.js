65819e9b7f42938acb5da911a05b92bf805ea317
ggodreau <godreau@gmail.com>
2019-01-30 14:33:57 -0600

    TouchableNativeFeedback working with in-border ripples

--- App.js
+++ App.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View, TouchableNativeFeedback, Text } from 'react-native';
 
 export default class ButtonBasics extends Component {
   _onPressButton() {
-    Alert.alert('You tapped the button!')
+    //Alert.alert('You tapped the button!')
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="OK!"
-            color="#841584"
-          />
+          <TouchableNativeFeedback
+              onPress={this._onPressButton}
+              background={TouchableNativeFeedback.Ripple('white', false)}>
+            <View style={{width: 150, height: 100, backgroundColor: 'gray'}}>
+              <Text style={{margin: 30}}>click me pls</Text>
+            </View>
+          </TouchableNativeFeedback>
         </View>
       </View>
     );
@@ -44,7 +29,9 @@ const styles = StyleSheet.create({
    justifyContent: 'center',
   },
   buttonContainer: {
-    margin: 20
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   alternativeLayoutButtonContainer: {
     margin: 20,
